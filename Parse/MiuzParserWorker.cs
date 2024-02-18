7908543d03bdfd6cb13b4b7a97d91f6e84e0f6ac
Влада <bobkovavlada302@gmail.com>
2024-02-18 22:46:52 +0300

    [master] [add] вывод информации в консоль

--- Parse/MiuzParserWorker.cs
+++ Parse/MiuzParserWorker.cs
@@ -1,12 +1,5 @@
-﻿using AngleSharp.Browser;
-using LittleOwl.Models;
+﻿using LittleOwl.Models;
 using LittleOwl.Parse.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LittleOwl.Parse
 {
--- Program.cs
+++ Program.cs
@@ -11,10 +11,19 @@ settings.Url = "catalog/chain/";
 settings.StartPoint = 1;
 settings.EndPoint = 8;
 
+Console.WriteLine($"\nНастройки");
+Console.WriteLine($"\nОсновной url: {settings.BaseUrl}");
+Console.WriteLine($"Префикс: {settings.Prefix}");
+Console.WriteLine($"Путь: {settings.Url}");
+Console.WriteLine($"Начальная страница: {settings.StartPoint} - " +
+    $"Конечная страница: {settings.EndPoint}\n");
+
 MiuzParserWorker worker = new(parser, settings);
 
+Console.WriteLine($"\nПроцес...");
 worker.Run();
 
+Console.WriteLine("\nСохранение...");
 string path = Environment.CurrentDirectory + @"\result.csv";
 
 using (var writer = new StreamWriter(path))
@@ -23,5 +32,4 @@ using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
     csv.WriteRecords(worker.Products);
 }
 
-Console.WriteLine($"\nСохранение...\n");
-Console.WriteLine($"Файл результатов по адресу: {path}");
+Console.WriteLine($"\nФайл результатов по адресу: {path}");
